6b8a6f7568535bcec3ae9837ca903602b82c1194
federico <f.casali@gmail.com>
2015-11-22 22:58:13 -0800

    iOS Geolocation ObjC sample

---
+++ iOS-Geolocation_ObjC/iOS-Geolocation_ObjC/AppDelegate.m
@@ -0,0 +1,71 @@
+//
+//  AppDelegate.m
+//  iOS-Geolocation_ObjC
+//
+//  Created by Federico Casali on 11/22/15.
+//  Copyright © 2015 Federico Casali. All rights reserved.
+//
+
+#import <Leanplum/Leanplum.h>
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+#ifdef DEBUG
+    LEANPLUM_USE_ADVERTISING_ID;
+    
+    // Set your AppID and AppKeys here
+    [Leanplum setAppId:@"" withDevelopmentKey:@""];
+#else
+    [Leanplum setAppId:@"" withProductionKey:@""];
+#endif
+    
+    // Registering the device for Push Notifications
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+    } else {
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes: UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge];
+    }
+#else
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes: UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge];
+#endif
+    
+    // Starts a new session and updates the app content from Leanplum.
+    [Leanplum start];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ iOS-Geolocation_ObjC/iOS-Geolocation_ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-Geolocation_ObjC
+//
+//  Created by Federico Casali on 11/22/15.
+//  Copyright © 2015 Federico Casali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
