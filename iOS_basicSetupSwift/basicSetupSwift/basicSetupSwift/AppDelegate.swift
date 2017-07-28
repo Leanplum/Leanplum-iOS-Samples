ec030a900ea28dbacc805c85a3438571a16f2fb2
Matthew Bell <mattbell@Matthews-MacBook-Pro.local>
2017-07-28 08:31:10 -0700

    fix(basicSetupSwift): Remove unnecessary lines from app delegate

--- iOS_basicSetupSwift/basicSetupSwift/basicSetupSwift/AppDelegate.swift
+++ iOS_basicSetupSwift/basicSetupSwift/basicSetupSwift/AppDelegate.swift
@@ -16,7 +16,6 @@ import Leanplum
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var speed: Float?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
